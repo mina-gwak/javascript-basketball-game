f8e907b99fd67bd19601e33f51df521edc5fa61a
Han-Joon-Hyeok <virq@naver.com>
2020-10-29 20:40:04 +0900

    Merged Mina & Joon

--- 준혁.js
+++ 준혁.js
@@ -15,9 +15,13 @@ hoopImage.src = "./images/hoop.png";
 var ball = new Image();
 ball.src = "./images/basketball.png";
 
-function drawBackground(){
+window.onload = function drawBackground(){
     bgCtx.drawImage(bgImage, 0, 0);
     bgCtx.drawImage(hoopImage, 1060, 400);
+    scoreArr = [0, 0, 0];
+    showTime();
+    createRanking();
+    startScreen();
 }
 
 const width = gCanvas.width;
@@ -36,61 +40,6 @@ let isFired = false;
 let isHitted = false;
 const GRAVITY_ACCELERATION = 0.098;
 let degree=0;
-
-// 공 그리기
-
-function drawBall(){
-    gCtx.drawImage(ball, ballX, ballY, 120, 120);
-
-}
-
-function draw(){
-    gCtx.clearRect(0,0,width,height);
-    arrowFrame();
-    moveArrow();
-    drawGauging();
-    drawGaugeBar();
-    drawBall();
-    drawBackground();
-
-    if(!isFired){
-        ballX = 100;
-        ballY = 630;
-    }
-    else{
-        if(ballY+ballVy < 0 || ballY + ballVy > height){
-            ballVy = - ballVy;
-        }
-        else if(ballX+ballVx < 0 || ballX + ballVx > width){
-            ballVx = - ballVx;
-        }
-        ballVy += 0.98;
-        ballX += ballVx;
-        ballY += ballVy;
-    }
-}
-
-function drawGaugeBar(){
-    gCtx.strokeRect(600,100,200,30);
-    gCtx.lineWidth = 3;
-    gCtx.font = "20px bold"
-    gCtx.fillText("파워 게이지", 650, 180);
-}
-
-function drawGauging(){
-    if(gauge<=200 && isCharging && !isFired){
-        gCtx.fillStyle = "#E67567";
-        gauge += 1;
-        gCtx.fillRect(600,100,gauge,30);
-    }
-    else if(!isCharging && isFired){
-        gCtx.fillStyle = "black";
-        gCtx.fillRect(600,100,0,30);
-    }
-    else if(isCharging){
-        gCtx.fillRect(600,100,200,30);
-    }
-};
 
 // 각도기 조절
 
@@ -139,6 +88,248 @@ function moveArrow() {
 
     degree += direction;
 }
+
+// 타이머
+
+// 타이머 영역 만들기
+
+var timeArea = document.createElement("div");
+timeArea.setAttribute("class", "timer area");
+document.getElementById("canvas-area").appendChild(timeArea);
+
+var sec = document.createElement("p");
+sec.setAttribute("class", "large");
+document.querySelector(".timer").appendChild(sec);
+
+var timerText = document.createElement("span");
+timerText.setAttribute("class", "bold");
+timerText.innerText = "SECOND";
+document.querySelector(".timer").appendChild(timerText);
+
+// 타이머
+
+var time = 30;
+
+function showTime() {
+  document.querySelector(".timer > p").innerText = time;
+}
+
+function gameTimer() {
+  var arrow = setInterval(moveArrow, 15);
+  var timer = setInterval(changeTime, 1000);
+  function changeTime() {
+    time -= 1;
+    if (time != 0) {
+      document.querySelector(".timer > p").innerText = time;
+    } else {
+      document.querySelector(".timer > p").innerText = time;
+      clearInterval(timer);
+      clearInterval(arrow);
+      time = 30;
+      endScreen();
+    }
+  }
+}
+
+// gameTimer();
+
+// 스코어 영역 만들기
+// 점수 카운트하는 부분이 구현되지 않아 임의로 값 넣어둠
+
+var scoreArea = document.createElement("div");
+scoreArea.setAttribute("class", "score area");
+document.getElementById("canvas-area").appendChild(scoreArea);
+
+var score = document.createElement("p");
+score.setAttribute("class", "large");
+score.innerText = 0;
+document.querySelector(".score").appendChild(score);
+
+var scoreText = document.createElement("span");
+scoreText.setAttribute("class", "bold");
+scoreText.innerText = "SCORE";
+document.querySelector(".score").appendChild(scoreText);
+
+// 시작 화면
+
+function startScreen() {
+    var darkDiv = document.createElement("div");
+    darkDiv.setAttribute("class", "dark-div");
+  
+    var largeBtn = document.createElement("p");
+    largeBtn.setAttribute("class", "div-btn");
+    largeBtn.innerHTML = "PRESS ENTER<br />TO START";
+    darkDiv.appendChild(largeBtn);
+  
+    document.getElementById("canvas-area").appendChild(darkDiv);
+  }
+  
+  // 엔딩 화면
+  
+  var currentScore;
+  
+  function endScreen() {
+    var darkDiv = document.createElement("div");
+    darkDiv.setAttribute("class", "dark-div end");
+  
+    var gameOver = document.createElement("h1");
+    gameOver.setAttribute("class", "end-heading");
+    gameOver.innerText = "GAME OVER";
+    darkDiv.appendChild(gameOver);
+  
+    var scoreBox = document.createElement("div");
+    scoreBox.setAttribute("class", "score-box");
+    darkDiv.appendChild(scoreBox);
+  
+    var scoreText = document.createElement("div");
+    scoreText.setAttribute("class", "score-text-box");
+    scoreBox.appendChild(scoreText);
+  
+    var score = document.createElement("p");
+    score.setAttribute("class", "score-text");
+    score.innerText = "SCORE";
+    scoreText.appendChild(score);
+  
+    var scoreNum = document.createElement("p");
+    scoreNum.setAttribute("class", "score-text");
+    // var currentScore = document.querySelector(".score p").innerText;
+    // 이미지랑 랭킹 업데이트 확인하려고 1로 지정해둠
+    currentScore = "1";
+    scoreNum.innerText = currentScore;
+    scoreText.appendChild(scoreNum);
+  
+    var best = document.createElement("p");
+    best.setAttribute("class", "score-text");
+    best.innerText = "BEST";
+    scoreText.appendChild(best);
+  
+    var bestNum = document.createElement("p");
+    bestNum.setAttribute("class", "score-text");
+    bestNum.innerText = (parseInt(currentScore) > parseInt(scoreArr[0]) ? currentScore : scoreArr[0]);
+    scoreText.appendChild(bestNum);
+  
+    // 새로운 베스트 스코어가 나오면 new 이미지 띄우기
+  
+    if (parseInt(scoreNum.innerText) > parseInt(scoreArr[0])) {
+      var newScore = document.createElement("img");
+      newScore.setAttribute("src", "./images/new.png");
+      newScore.setAttribute("class", "new-score");
+      scoreBox.appendChild(newScore);
+    }
+  
+    var smallBtn = document.createElement("p");
+    smallBtn.setAttribute("class", "div-btn smBtn");
+    smallBtn.innerHTML = "PRESS ENTER TO RESTART";
+    darkDiv.appendChild(smallBtn);
+  
+    document.getElementById("canvas-area").appendChild(darkDiv);
+  
+    // 랭킹 업데이트 
+  
+    for (var i = 0; i < scoreArr.length; i++) {
+      if (scoreArr.includes(parseInt(currentScore))) break;
+      if (parseInt(currentScore) > parseInt(scoreArr[i])) {
+        scoreArr.splice(i, 0, parseInt(currentScore));
+        scoreArr.pop();
+        break;
+      }
+    }
+  }
+  
+  // 게임 시작 & 재시작
+  
+  function startGame(event) {
+    if (event.keyCode == 13) {
+      event.preventDefault();
+      var div = document.querySelector(".dark-div");
+      div.parentNode.removeChild(div);
+      var rank = document.querySelector(".ranking");
+      rank.parentNode.removeChild(rank);
+      showTime();
+      createRanking();
+      gameTimer();
+    }
+  }
+  
+  document.addEventListener("keydown", startGame);
+  
+  // 랭킹 테이블
+  
+  var scoreArr; // onload에서 초기화했음
+  
+  function createRanking() {
+    var ranking = document.createElement("table");
+    var rankingHead = document.createElement("thead");
+    var rankingBody = document.createElement("tbody");
+    ranking.appendChild(rankingHead);
+    ranking.appendChild(rankingBody);
+    ranking.setAttribute("class", "ranking");
+    for (var i = 0; i < 4; i++) {
+      if (!i) {
+        rankingHead.innerHTML += "<tr><th>순위</th><th>점수</th></tr>";
+      } else {
+        rankingBody.innerHTML += "<tr><td>"+i+"등</td><td>"+scoreArr[i-1]+"점</td></tr>";
+      }
+    }
+    document.getElementById("canvas-area").appendChild(ranking);
+  }
+
+// 공 그리기
+
+function drawBall(){
+    gCtx.drawImage(ball, ballX, ballY, 120, 120);
+
+}
+
+function draw(){
+    gCtx.clearRect(0,0,width,height);
+    arrowFrame();
+    moveArrow();
+    drawGauging();
+    drawGaugeBar();
+    drawBall();
+    // drawBackground();
+
+    if(!isFired){
+        ballX = 100;
+        ballY = 630;
+    }
+    else{
+        if(ballY+ballVy < 0 || ballY + ballVy > height){
+            ballVy = - ballVy;
+        }
+        else if(ballX+ballVx < 0 || ballX + ballVx > width){
+            ballVx = - ballVx;
+        }
+        ballVy += 0.98;
+        ballX += ballVx;
+        ballY += ballVy;
+    }
+}
+
+function drawGaugeBar(){
+    gCtx.strokeRect(600,100,200,30);
+    gCtx.lineWidth = 3;
+    gCtx.font = "20px bold"
+    gCtx.fillText("파워 게이지", 650, 180);
+}
+
+function drawGauging(){
+    if(gauge<=200 && isCharging && !isFired){
+        gCtx.fillStyle = "#E67567";
+        gauge += 1;
+        gCtx.fillRect(600,100,gauge,30);
+    }
+    else if(!isCharging && isFired){
+        gCtx.fillStyle = "black";
+        gCtx.fillRect(600,100,0,30);
+    }
+    else if(isCharging){
+        gCtx.fillRect(600,100,200,30);
+    }
+};
+
+
 
 // 스페이스바 입력
 
